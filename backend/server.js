69b984c42f47fc55891cfd88f165a080698bac95
Sufian <167395097+Sufian898@users.noreply.github.com>
2025-12-27 16:02:32 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'https://easycomission.com',
 'https://www.easycomission.com',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://manditradeerp.com'
 ];
 
 // --- FIXED CORS (Vercel-Compatible) ---
